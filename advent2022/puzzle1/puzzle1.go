9624d1fea121985ae43c375a551671ec519f716b
Ashish Nair <nairashu@gmail.com>
2022-12-02 02:58:40 +0000

    Solution for Day 1 of Advent 2022

--- advent2022/puzzle1/puzzle1.go
+++ advent2022/puzzle1/puzzle1.go
@@ -5,11 +5,13 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"sort"
+	"strconv"
 )
 
 func RunPuzzle1() {
 	// open file
-	f, err := os.Open("Input1.txt")
+	f, err := os.Open("./puzzle1/Input1.txt")
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -19,10 +21,41 @@ func RunPuzzle1() {
 	// read the file line by line using scanner
 	scanner := bufio.NewScanner(f)
 
+	caloriesByElves := make(map[int]int)
+	calories := make([]int, 0, 10)
+	calorieTotal := 0
+	elfNumber := 1
+	maxCalorie := 0
+
 	for scanner.Scan() {
 		// do something with a line
-		fmt.Printf("line: %s\n", scanner.Text())
+		//fmt.Printf("line: %s\n", scanner.Text())
+		if scanner.Text() == "" {
+			caloriesByElves[elfNumber] = calorieTotal
+			calories = append(calories, calorieTotal)
+			if maxCalorie < calorieTotal {
+				maxCalorie = calorieTotal
+			}
+			calorieTotal = 0
+			elfNumber++
+		} else {
+			curr, err := strconv.Atoi(scanner.Text())
+			if err != nil {
+				log.Fatal(err)
+			}
+			calorieTotal = calorieTotal + curr
+		}
 	}
+
+	fmt.Println("Max Calorie is: ", maxCalorie)
+
+	sort.Sort(sort.Reverse(sort.IntSlice(calories)))
+	top3Total := 0
+	for i := 0; i < 3; i++ {
+		top3Total = top3Total + calories[i]
+	}
+
+	fmt.Println("Total of top 3 Max Calorie is: ", top3Total)
 
 	if err := scanner.Err(); err != nil {
 		log.Fatal(err)
