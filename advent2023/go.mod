e67ea1d0836f6574fd43c399024bed17755eb0ad
Ashish Nair <asn@microsoft.com>
2023-12-01 16:36:41 +0000

    Added the 2023 folder for some advent of code fun

---
+++ advent2023/go.mod
@@ -0,0 +1,8 @@
+module github.com/nairashu/Go-Lang-Practice/adevent2022
+
+go 1.19
+
+require (
+	github.com/golang-collections/collections v0.0.0-20130729185459-604e922904d3
+	golang.org/x/exp v0.0.0-20221217163422-3c43f8badb15
+)
---
+++ advent2023/main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+
+	// "github.com/nairashu/Go-Lang-Practice/adevent2022/puzzle1"
+	// "github.com/nairashu/Go-Lang-Practice/adevent2022/puzzle2"
+	// "github.com/nairashu/Go-Lang-Practice/adevent2022/puzzle3"
+	// "github.com/nairashu/Go-Lang-Practice/adevent2022/puzzle4"
+	// "github.com/nairashu/Go-Lang-Practice/adevent2022/puzzle5"
+	"github.com/nairashu/Go-Lang-Practice/adevent2022/puzzle6"
+)
+
+func main() {
+	fmt.Println("Hello Advent 2022")
+	// puzzle1.RunPuzzle1()
+	// puzzle2.RunPuzzle2()
+	// puzzle3.RunPuzzle3()
+	// puzzle3.RunPuzzle3Part2()
+	// puzzle4.RunPuzzle4()
+	// puzzle5.RunPuzzle5()
+	puzzle6.RunPuzzle6()
+}
